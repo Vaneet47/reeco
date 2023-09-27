8a36a23f56dadc72a2e05af61d42bf28b7c8c6f7
Vaneet Gupta <vaneet.gupta47@gmail.com>
2023-09-27 10:46:09 +0530

    order-data-head

--- src/components/orderData/OrderData.js
+++ src/components/orderData/OrderData.js
@@ -2,7 +2,21 @@ import React from 'react';
 import './orderData.css';
 
 function OrderData() {
-  return <div className='order-data'>OrderData</div>;
+  return (
+    <div className='order-data'>
+      <div className='head'>
+        <div className='search'>
+          <input placeholder='Search...' />
+        </div>
+        <div className='add-print'>
+          <button className='back add-item'>Add item</button>
+          <span class='material-symbols-outlined icon print'>print</span>
+        </div>
+      </div>
+
+      <div>Table</div>
+    </div>
+  );
 }
 
 export default OrderData;
